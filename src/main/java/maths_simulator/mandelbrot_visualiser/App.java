fe8a43aec730ba5f819ab0063ee61d06266548ef
Alexander Kingston <akingston@securiport.com>
2018-01-08 11:30:18 -0500

    First commit

---
+++ src/main/java/maths_simulator/mandelbrot_visualiser/App.java
@@ -0,0 +1,13 @@
+package maths_simulator.mandelbrot_visualiser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
